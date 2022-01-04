0ae409e99fdf39fc5ad1bb5da60e2c993692749b
jaishanu <jaishanu2410@gmail.com>
2022-01-04 21:52:14 +0530

    fdb

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('/');
+const socket = io(' https://intense-waters-45802.herokuapp.com');
 const chatInputBox = document.getElementById("chat_message");
 const all_messages = document.getElementById("all_messages");
 const main__chat__window = document.getElementById("main__chat__window");
